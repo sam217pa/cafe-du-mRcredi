1ac2e8456ff85dd47e14e0e5e986e04e55c65e7c
sam217pa <samuel.barreto8@gmail.com>
2017-01-25 10:48:29 +0100

    update page d'accueil

--- index.R
+++ index.R
@@ -22,6 +22,11 @@ rmarkdown::render("/Users/samuelbarreto/prof/cafe-du-mRcredi/index.R",
 ## */
 
 #' - premier cours sur le tidyverse et tout ça :
-#'   [ici](https://sam217pa.github.io/cafe-du-mRcredi/01-session-chauffe.html)
-#' - second cours sur la grammaire des graphiques :
+#'   [ici](https://sam217pa.github.io/cafe-du-mRcredi/01-session-chauffe.html) -
+#'   second cours sur la grammaire des graphiques :
 #'   [ici](https://sam217pa.github.io/cafe-du-mRcredi/02-premiers-graphiques.html)
+#'   - troisième cours sur l'importation et le nettoyage de données :
+#'   [ici](https://sam217pa.github.io/cafe-du-mRcredi/03-data-import-and-clean.html)
+#'   - quatrième cours sur la combinaison de données et l'analyse depuis
+#'   l'import aux premiers graphiques :
+#'   [ici](https://sam217pa.github.io/cafe-du-mRcredi/04-recapitulatif.html)
